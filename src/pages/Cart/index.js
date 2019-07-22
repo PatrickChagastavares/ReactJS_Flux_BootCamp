9f4eb979d90ddfc8841929d13e818afd5ecd4b1a
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-22 18:57:53 -0300

    create: configuring routes and styles global

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Cart() {
+  return (
+    <div>
+      <h1>page Cart</h1>
+    </div>
+  );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Page Main</h1>
+    </div>
+  );
+}
