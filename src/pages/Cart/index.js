8f47b017a752d443689e6f7391f2438ccf2e2843
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-23 09:54:07 -0300

    update: adding styles page Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div>
-      <h1>page Cart</h1>
-    </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis Muito mass</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly Value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,00</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
