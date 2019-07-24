d1b070c89af951eaa804a43220b4bcd13d9f0810
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-24 10:03:02 -0300

    update: adding and configuring React-Toastify and configuring search stock in update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
-import "./config/ReactotronConfig"
+import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header/index';
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
