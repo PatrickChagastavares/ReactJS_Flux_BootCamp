8599d2f94c3c78f26d18d0ef3da9b966fb4463ea
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-24 12:31:25 -0300

    create: converting class to function with Hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -36,5 +37,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
     'no-console': ["error", { allow: ["tron"] }],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
